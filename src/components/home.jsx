6058b5ad77a8a3ee352c62c5f0cfef7230169b6c
nikhilsinha <nikhilsinha822@gmail.com>
2022-10-27 23:53:17 +0530

    first commit

---
+++ src/components/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home(){
+    return <div className="Home">
+         <h1 className="introTxt">Get the best meal at your footsteps!</h1>
+         <button className="btn">B O O K - N O W</button>
+    </div>
+}
+
+export default Home; 
