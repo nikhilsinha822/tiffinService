d3eff980d05440ecfc86b3d16ca122e8150d055a
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2022-10-28 00:06:46 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Home from "./components/home"
+import Navbar from "./components/navbar"
+import Menu from "./components/menu"
+
+function App(){
+   return <div>
+        <Navbar /> 
+        <Home />
+        <Menu />
+    </div>
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar(){
+return <div className="navbar"><ul>
+    <li className="navitem"><img className="navimage" src="images/Screenshot_2022-10-15_131804-removebg-preview.png" /></li>
+    <li className="navitem"><a class="navlink">CONTACT</a></li>
+    <li className="navitem"><a class="navlink">MENU</a></li>
+    <li className="navitem"><a class="navlink">HOME</a></li> 
+</ul></div>
+}
+
+export default Navbar
